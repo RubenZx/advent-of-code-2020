a1af85564f1059df0173725c2958385e0d6dcce1
RubenZx <ruben.mondom@gmail.com>
2020-12-04 16:45:33 +0100

    :zap: refactor code to day1

--- src/day1/part1.mjs
+++ src/day1/part1.mjs
@@ -4,17 +4,14 @@ import { fileURLToPath } from 'url';
 
 // read the file
 const __dirname = dirname(fileURLToPath(import.meta.url));
-const initialData = fs.readFileSync(__dirname + "/input.txt", 'utf8').split('\n');
+const data = fs.readFileSync(__dirname + "/input.txt", 'utf8').split('\n');
 
 let found = false, value, element;
-for (let i = 0; i < initialData.length && !found; i++) {
-    const data = [...initialData]
+for (let i = 0; i < data.length && !found; i++) {
     element = data[i]; // take the i-element
-    data.splice(i, 1); // remove from data
-
     // find the element that sum 2020 with the rest
     value = data.find((value) => +value + +element === 2020);
-    found = value ? true : false; // if found => stop, else => continue
+    if (value) { found = true } // if found => stop
 }
 
 console.log({ value, element, prod: value * element })
